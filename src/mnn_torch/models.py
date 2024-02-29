08efb7b265b07cf00e479c3cad1119de540d7f33
hyperionx <mr_vcv@hotmail.co.uk>
2024-02-29 21:26:30 +0000

    added CNN models

--- src/mnn_torch/models.py
+++ src/mnn_torch/models.py
@@ -7,6 +7,49 @@ from mnn_torch.layers import MemristorLinearLayer
 
 
 # Define Network
+
+class SNN(nn.Module):
+    """Basic memrisitive model with spiking neural network"""
+
+    def __init__(
+        self,
+        device,
+        num_inputs,
+        num_hidden,
+        num_outputs,
+        num_steps,
+        beta,
+    ):
+        super().__init__()
+
+        self.num_steps = num_steps
+
+        # Initialize layers
+        self.fc1 = nn.Linear(num_inputs, num_hidden, device=device)
+        self.lif1 = snn.Leaky(beta=beta)
+        self.fc2 = nn.Linear(num_hidden, num_outputs, device=device)
+        self.lif2 = snn.Leaky(beta=beta)
+
+    def forward(self, x):
+        # Initialize hidden states at t=0
+        mem1 = self.lif1.init_leaky()
+        mem2 = self.lif2.init_leaky()
+
+        # Record the final layer
+        spk2_rec = []
+        mem2_rec = []
+
+        for _ in range(self.num_steps):
+            cur1 = self.fc1(x)
+            spk1, mem1 = self.lif1(cur1, mem1)
+            cur2 = self.fc2(spk1)
+            spk2, mem2 = self.lif2(cur2, mem2)
+            spk2_rec.append(spk2)
+            mem2_rec.append(mem2)
+
+        return torch.stack(spk2_rec, dim=0), torch.stack(mem2_rec, dim=0)
+
+
 class MSNN(nn.Module):
     """Basic memrisitive model with spiking neural network"""
 
@@ -19,16 +62,20 @@ class MSNN(nn.Module):
         num_steps,
         beta,
         memrisitive_config,
+
+
     ):
         super().__init__()
 
         self.num_steps = num_steps
 
         # Initialize layers
+        # self.fc1 = nn.Linear(num_inputs, num_hidden, device=device)
         self.fc1 = MemristorLinearLayer(
             device, num_inputs, num_hidden, memrisitive_config
         )
         self.lif1 = snn.Leaky(beta=beta)
+        # self.fc2 = nn.Linear(num_hidden, num_outputs, device=device)
         self.fc2 = MemristorLinearLayer(
             device, num_hidden, num_outputs, memrisitive_config
         )
@@ -53,8 +100,52 @@ class MSNN(nn.Module):
 
         return torch.stack(spk2_rec, dim=0), torch.stack(mem2_rec, dim=0)
 
+class CSNN(nn.Module):
+    def __init__(
+        self,
+        device,
+        beta,
+        spike_grad,
+        batch_size,
+        num_kernels,
+        num_conv1,
+        num_conv2,
+        max_pooling,
+        num_hidden,
+        num_outputs,
+    ):
+        super().__init__()
 
-class MSCNN(nn.Module):
+        self.batch_size = batch_size
+        self.max_pooling = max_pooling
+
+        # Initialize layers
+        self.conv1 = nn.Conv2d(1, num_conv1, num_kernels, device=device)
+        self.lif1 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+        self.conv2 = nn.Conv2d(num_conv1, num_conv2, num_kernels, device=device)
+        self.lif2 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+        self.fc1 = nn.Linear(num_hidden, num_outputs, device=device)
+        self.lif3 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+
+    def forward(self, x):
+        # Initialize hidden states and outputs at t=0
+        mem1 = self.lif1.init_leaky()
+        mem2 = self.lif2.init_leaky()
+        mem3 = self.lif3.init_leaky()
+
+        cur1 = F.max_pool2d(self.conv1(x), self.max_pooling)
+        spk1, mem1 = self.lif1(cur1, mem1)
+
+        cur2 = F.max_pool2d(self.conv2(spk1), self.max_pooling)
+        spk2, mem2 = self.lif2(cur2, mem2)
+
+        cur3 = self.fc1(spk2.view(self.batch_size, -1))
+        spk3, mem3 = self.lif3(cur3, mem3)
+
+        return spk3, mem3
+
+
+class MCSNN(nn.Module):
     def __init__(
         self,
         device,
@@ -79,6 +170,7 @@ class MSCNN(nn.Module):
         self.lif1 = snn.Leaky(beta=beta, spike_grad=spike_grad)
         self.conv2 = nn.Conv2d(num_conv1, num_conv2, num_kernels, device=device)
         self.lif2 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+        # self.fc1 = nn.Linear(num_hidden, num_outputs, device=device)
         self.fc1 = MemristorLinearLayer(
             device, num_hidden, num_outputs, memrisitive_config
         )
